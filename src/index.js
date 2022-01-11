2e107ec3550830acc25ccd7f9c3bb64782b23f22
tarik <tchubarko@gmail.com>
2022-01-11 17:01:23 +0200

    first react module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
-import styles from './styles.module.css'
+import BotSubscriber from "./components/BotSubscriber";
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const MesengerBot = ({
+                              type = null,
+                              label = 'Привязать',
+                              className, host = '',
+                              token = ''
+                            }) => {
+  switch (type) {
+    case 'telegram':
+      return <BotSubscriber label={label} className={className} host={host} token={token} provider={'telegram'}/>
+      break;
+    case 'whatsapp':
+      return <BotSubscriber label={label} className={className} host={host} token={token} provider={'whatsapp'}/>
+      break;
+    default:
+      return <button className={className}>{label}</button>
+  }
 }
